bf5f00d008352d407c2d6512343afa6606b170d4
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-12-19 10:22:59 +0100

    Styled nearby page

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,26 +1,229 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Tab3.css';
+import "./Tab3.css";
 import useLocation from "../hooks/useLocation";
-import GeolocationButton from '../components/Geolocationbutton';
+import GeolocationButton from "../components/Geolocationbutton";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonImg,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonList,
+  IonItemSliding,
+  IonItemOptions,
+  IonItemOption,
+} from "@ionic/react";
+import { useStorage } from "../components/useStorage";
+import { useCallback, useState, useRef } from "react";
+import GasStationCard from "../components/GasStationCard";
+import React from "react";
+import useStation from "../hooks/useStation";
+import useProducto from "../hooks/useProducto";
 
 const Tab3: React.FC = () => {
   const location = {
-    //TRAER ESTOS DATOS DE LA LOCALIZACION DEL USUARIO 
-    lat: 37.8132667, 
-    lng: -5.0137913
-  }
-  const response = useLocation(location);
-  return (
-    <IonPage>
-      <IonContent fullscreen>
-        <h1>{response?.locality}</h1>
-        <h1>{response?.region}</h1>
-        <h1>{response?.area}</h1>
-      </IonContent>
+    //TRAER ESTOS DATOS DE LA LOCALIZACION DEL USUARIO
+    lat: 37.8132667,
+    lng: -5.0137913,
+  };
 
-      <GeolocationButton ></GeolocationButton>
-    </IonPage>
-  );                                                                           
+  const getSearchingInitialState = () => {
+    const Searching = false;
+    return Searching;
+  };
+
+  const [Searching, setSearching] = useState(getSearchingInitialState);
+  const handleSearchingChange = () => {
+    setSearching(!Searching);
+  };
+
+  const getIDProductoInitialState = () => {
+    const IDProducto = "-";
+    return IDProducto;
+  };
+
+  const [IDProducto, setIDProducto] = useState(getIDProductoInitialState);
+  const Productos = useProducto();
+  const handleProductoChange = (e: any) => {
+    setIDProducto(e.target.value);
+  };
+
+  const getIDEESSInitialState = () => {
+    const IDEESS = "-";
+    return IDEESS;
+  };
+
+  const [IDEESS, setIDEESS] = useState(getIDEESSInitialState);
+  const handleEESSChange = (e: any) => {
+    setIDEESS(e.target.value);
+  };
+
+  const Stations = useStation("2063", IDProducto);
+
+  //////////////////////////////////////
+  const { gass, addGas, removeGas } = useStorage();
+  const [place, setPlace] = useState("");
+  const ionList = useRef(null as any);
+
+  const createGas = async (id1: string, id2: string, id3: string | null) => {
+    await addGas(id1, id2, id3);
+    setPlace("");
+  };
+
+  const deleteGas = async (id: string) => {
+    //TODO
+    removeGas(id);
+    ionList.current.closeSlidingItems();
+  };
+  /////////////////////////////////////////
+
+  const response = useLocation(location);
+
+  if (!Searching) {
+    return (
+      <IonPage>
+        <IonContent fullscreen>
+          <header className="App-header">
+            <img
+              className="App-image"
+              src="assets/images/logo.png"
+              alt="logo"
+            ></img>
+          </header>
+          <body className="App-body">
+            <div className="App-formcolumn">
+              <div className="Form-block">
+                <p className="Form-title">
+                  <b>
+                    <i>GASOLINERAS CERCA DE TI</i>
+                  </b>
+                </p>
+              </div>
+              <img
+                className="Find-nearby-image"
+                src="assets/images/nearby.png"
+                alt="logo"
+              ></img>
+              <div className="Form-block">
+                <p className="Form-title">
+                  <b>
+                    <i>COMBUSTIBLE</i>
+                  </b>
+                </p>
+                <select
+                  value={IDProducto}
+                  className="Form-select"
+                  onChange={handleProductoChange}
+                >
+                  <option value={"-"}>-</option>;
+                  {Productos.map((Producto) => {
+                    return (
+                      <option value={Producto.IDProducto}>
+                        {Producto.NombreProducto}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <div className="Form-block">
+                <p className="Form-title">
+                  <b>
+                    <i>ESTACIÓN DE SERVICIO</i>
+                  </b>
+                </p>
+                <select
+                  value={IDEESS}
+                  className="Form-select"
+                  onChange={handleEESSChange}
+                >
+                  <option value={"-"}>-</option>;
+                  <option value={"BP"}>BP</option>;
+                  <option value={"CEPSA"}>Cepsa</option>;
+                  <option value={"GALP"}>Galp</option>;
+                  <option value={"PETRONOR"}>Petronor</option>;
+                  <option value={"PLENOIL"}>Plenoil</option>;
+                  <option value={"REPSOL"}>Repsol</option>;
+                </select>
+              </div>
+              <br></br>
+              <div className="Form-block">
+                <button
+                  className="Form-button"
+                  onClick={() => handleSearchingChange()}
+                >
+                  <b>
+                    <i>BUSCAR</i>
+                  </b>
+                </button>
+              </div>
+            </div>
+          </body>
+        </IonContent>
+      </IonPage>
+    );
+  } else {
+    return (
+      <IonPage>
+        <IonContent fullscreen>
+          <header className="App-header">
+            <img
+              className="App-image"
+              src="assets/images/logo.png"
+              alt="logo"
+            ></img>
+          </header>
+          <body className="App-body">
+            <div className="First-Row">
+              <p className="Stations-Title">
+                <b>
+                  <i>RESULTADOS</i>
+                </b>
+              </p>
+              <button
+                className="New-Search-button"
+                onClick={() => handleSearchingChange()}
+              >
+                <b>
+                  <i>NUEVA BÚSQUEDA</i>
+                </b>
+              </button>
+            </div>
+            {Stations.map((Station, key) => {
+              if (
+                Station.Rótulo.search(IDEESS.toUpperCase()) != -1 ||
+                IDEESS === "-"
+              ) {
+                return (
+                  <IonList ref={ionList}>
+                    <IonItemSliding key={key}>
+                      <IonItem>
+                        <GasStationCard gasStation={Station} />
+                      </IonItem>
+                      <IonItemOptions
+                        side="start"
+                        onClick={() =>
+                          createGas(
+                            Station.IDEESS,
+                            Station.IDMunicipio,
+                            Station.IDProducto
+                          )
+                        }
+                      >
+                        <IonItemOption>Favoritos</IonItemOption>
+                      </IonItemOptions>
+                    </IonItemSliding>
+                  </IonList>
+                );
+              }
+            })}
+          </body>
+        </IonContent>
+      </IonPage>
+    );
+  }
 };
 
 export default Tab3;
